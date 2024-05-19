c8a1a1d202253464960578d3a7294ebe4b36957f
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-05-19 21:45:31 +0400

    fix

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const fs = require('fs')
 const arguments = process.argv;
 
-fs.readFile(`${arguments[1]}`, 'utf-8', (err, data) => {
+fs.readFile(`${arguments[2]}`, 'utf-8', (err, data) => {
     if (err) {
         console.log(err);
     }
